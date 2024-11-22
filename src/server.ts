28866d80f024055c3b3b74e1c40eca1645775170
fborovsak <fernando.borovsak@outlook.com>
2024-11-22 12:51:26 -0300

    Agregado pug como template render

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import Fastify from "fastify"
 import rutas from "./rutas"
+import fastifyView from "@fastify/view"
+import pug from "pug"
+
 
 export default function createServer() {
     const fastify = Fastify({
         logger: true
     })
+
+    fastify.register(fastifyView, {
+        engine: {
+          pug
+        },
+        templates: "./src/views"
+      })
+      
+
+
     fastify.register(rutas)
     return fastify
 }
