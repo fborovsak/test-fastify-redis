434d7656424e590f3ccfd6027e27b0e168368d0a
fborovsak <fernando.borovsak@outlook.com>
2024-11-22 20:00:23 -0300

    Estructura basica de templates armada

--- src/rutas.ts
+++ src/rutas.ts
@@ -1,7 +1,44 @@
 import { FastifyInstance } from "fastify"
 
+type TRegisterPostBody = {
+    fullname:string
+    email: string
+    password: string
+}
+
+type TLoginPostBody = {
+    email: string
+    password: string
+}
+
 export default function rutas(fastify: FastifyInstance) {
     fastify.get("/", (req, res) => {
         res.view("root.pug")
+    })
+
+    fastify.get("/register", (req, res) => {
+        res.view("register.pug", {titulo: "Registro de usuarios"})
+    })
+
+    fastify.post("/register", (req, res) => {
+        const body = req.body as Partial<TRegisterPostBody>
+        res.redirect("/registerDone")
+    })
+
+    fastify.get("/registerDone", (req,res) => {
+        res.view("thankyou.pug", {titulo: "Registro completado"})
+    })
+
+    fastify.get("/login", (req, res) => {
+        res.view("login.pug", {titulo: "Login de usuarios registrados"})
+    })
+
+    fastify.post("/login", (req, res) => {
+        const body = req.body as Partial<TLoginPostBody>
+        res.redirect("/user")
+    })
+
+    fastify.get("/user", (req, res) => {
+        res.view("user.pug", {titulo: "Centro de usuarios"})
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import Fastify from "fastify"
 import rutas from "./rutas"
 import fastifyView from "@fastify/view"
 import pug from "pug"
+import fastifyFormbody from "@fastify/formbody"
 
 
 export default function createServer() {
     const fastify = Fastify({
         logger: true
     })
+
+    fastify.register(fastifyFormbody)
 
     fastify.register(fastifyView, {
         engine: {
